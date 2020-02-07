7f285d22cf3ed39e9d1e125b279f0048f0a20693
Hylke Middel <h.middel@st.hanze.nl>
2020-02-07 11:38:46 +0100

    Add min-max scaling and sd based filtering to visualization

--- visualization.py
+++ visualization.py
@@ -5,13 +5,20 @@ import numpy as np
 if __name__ == '__main__':
     df = pd.read_csv("data/binding_data.tsv", sep="\t")
     df = df[df["IC50 (nm)"] != 0]
-    df = df[df["IC50 (nm)"] < 1000000]
     IC50s = df["IC50 (nm)"]
     print(IC50s[:5])
-    scaled = np.log10(IC50s)
-    print(sum([i > 20 for i in scaled]))
+    log_scaled = np.log10(IC50s)
+    print(sum([i > 20 for i in log_scaled]))
+    mean = np.mean(log_scaled)
+    sd = np.std(log_scaled)
+    print(f"Mean: {mean}")
+    print(f"Standard Deviation: {sd}")
+    filtered = [i for i in log_scaled if mean - 3 * sd < i < mean + 3 * sd]
+    minimum = min(filtered)
+    maximum = max(filtered)
+    min_max_scaled = [(i-minimum)/(maximum-minimum) for i in filtered]
     figure = plt.figure()
     figure.suptitle('Repartition of kinase families after embedding', fontsize=16)
-    figure = plt.hist(scaled, color = 'blue', edgecolor = 'black',
+    figure = plt.hist(min_max_scaled, color = 'blue', edgecolor = 'black',
          bins = 100)
     plt.show()
