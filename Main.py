d87d06accd087f012220faef5523e3c1703c1a2f
CoMorvan <coren.morvan@gmail.com>
2020-02-19 14:44:34 +0100

    Changed to fit the output of the GA

--- Main.py
+++ Main.py
@@ -8,12 +8,13 @@ from CustomLayers import BiLSTMSelfAttentionLayer
 from Protein_embedding import embed_protein
 from smiles_embedding import embed_smiles
 from visualization import min_max_scale
+from data_extraction import get_info
 
 if __name__ == '__main__':
     # General settings
     batch_size = 64
     # Dataset settings
-    dataset_fraction = 0.1
+    dataset_fraction = 1
     cross_validation_number = 5
     # Protein embedding settings
     prot_embedding_dim = 50
@@ -29,9 +30,24 @@ if __name__ == '__main__':
     epochs = 3
 
     X = []
-    data = pd.read_csv("data/binding_data_final.tsv", sep="\t")
+    sdf_test = ["data/Angel_dataset/scorp/fixed-conformers_3d_scorp (" + str(j + 1) + ").sdf" for j in range(
+        71)]  + ["data/Angel_dataset/scorp/fixed-conformers_3d_3d_scorp (" + str(i+1) + ").sdf" for i in range(23)]
+    pdb_test = ["data/Angel_dataset/Protein/bs_protein (" + str(i + 1) + ").pdb" for i in range(94)]
+    defect, info = get_info(sdf_test, "data/Angel_dataset/pro.pdb", pdb_test)
+    data_angel = pd.DataFrame(info)
+    data_angel = data_angel.dropna()
+
+    sdf_test = ["data/Xin_dataset/scorp/fixed-conformers_3d_scorp (" + str(j + 1) + ").sdf" for j in range(
+        71)] + ["data/Xin_dataset/scorp/fixed-conformers_3d_3d_scorp (" + str(i + 1) + ").sdf" for i in range(23)]
+    pdb_test = ["data/Xin_dataset/Protein/bs_protein (" + str(i + 1) + ").pdb" for i in range(82)]
+    defect, info = get_info(sdf_test, "data/Xin_dataset/pro.pdb", pdb_test)
+    data_xin = pd.DataFrame(info)
+    data_xin = data_angel.dropna()
+    data = data_angel.append(data_angel)
+
     data = data.sample(frac=dataset_fraction)
     data = np.array_split(data, cross_validation_number)
+
 
     for i in range(cross_validation_number) :
         test_data = pd.DataFrame(data[i])
@@ -39,11 +55,10 @@ if __name__ == '__main__':
         del dataset[i]
         train_data = pd.concat(dataset)
         del dataset
-        train_smiles, test_smiles = train_data['Ligand SMILES'], test_data['Ligand SMILES']
-        train_prot, test_prot = train_data["BindingDB Target Chain  Sequence"], test_data[
-        "BindingDB Target Chain  Sequence"]
-        train_IC, test_IC = np.array(min_max_scale(train_data["IC50 (nm)"])), np.array(
-            min_max_scale(test_data["IC50 (nm)"]))
+        train_smiles, test_smiles = train_data['SMILES'], test_data['SMILES']
+        train_prot, test_prot = train_data["Sequence"], test_data["Sequence"]
+        train_IC, test_IC = np.array(min_max_scale(train_data["score"])), np.array(
+            min_max_scale(test_data["score"]))
 
         del train_data, test_data
 
