84c8163b77eae4a9da24b559430be283bfff43c1
CoMorvan <coren.morvan@gmail.com>
2020-03-11 14:48:58 +0100

    Reformating code

--- Protein_model_training.py
+++ Protein_model_training.py
@@ -4,12 +4,12 @@ from gensim.models import Word2Vec
 from gensim.test.utils import get_tmpfile
 
 
-def protein_train(dims, data, n, window_size, negative_size):
+def protein_train(dims, prot_seq, n, window_size, negative_size):
     """
     word2vec method to embed protein sequence, based on
      https://github.com/dqwei-lab/SPVec/blob/master/examples/Biochemical%20implications%20of%20ProtVec%20features.ipynb
     :param dims: length of the output vectors
-    :param data: input data in the form of an iterable containing protein sequences
+    :param prot_seq: input data in the form of an iterable containing protein sequences
     :param n: the k-mer size to use
     :param window_size: maximum distance between the current and predicted word within a sentence
     :param negative_size: If > 0, negative sampling will be used, the int for negative specifies how many "noise words"
@@ -17,9 +17,9 @@ def protein_train(dims, data, n, window_size, negative_size):
     :save the model in a file
     """
     texts = []
-    prot_words = [[] for _ in range(len(list(data)))]
+    prot_words = [[] for _ in range(len(list(prot_seq)))]
     get_tmpfile("protein_embedding.model")
-    for (j, document) in enumerate(list(data)):
+    for (j, document) in enumerate(list(prot_seq)):
         prot_words[j] = [word for word in re.findall(r'.{' + str(n) + '}', document)]
         texts += [prot_words[j]]
     model = Word2Vec(texts, size=dims, window=window_size, min_count=1, negative=negative_size, sg=1, sample=0.001,
@@ -29,7 +29,7 @@ def protein_train(dims, data, n, window_size, negative_size):
 
 if __name__ == "__main__":
     data = set()
-    with open("data/BindingDB_All.tsv", encoding='utf-8') as i:
+    with open("data/DTI/BindingDB_All.tsv", encoding='utf-8') as i:
         for line in i:
             splitline = line.split("\t")
             data.add(splitline[37])
