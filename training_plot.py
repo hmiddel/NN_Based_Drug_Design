bc0da4af0ff3b44479d588dc9bb54f4361f79384
CoMorvan <coren.morvan@gmail.com>
2020-03-12 13:34:49 +0100

    Adjusted the axis

--- training_plot.py
+++ training_plot.py
@@ -5,21 +5,24 @@ import numpy as np
 
 
 def plots(x_values, y_values, label=None, save=False):
+    mini = min(min(x_values), min(y_values))
+    maxi = max(max(x_values), max(y_values))
     fig = plt.figure(figsize=(5, 12))
 
     ax1 = fig.add_subplot(311)
     ax1.set_title('real')
     ax1.hist(x_values)
-    ax1.set_xlim(min(min(x_values), min(y_values)), max(max(x_values), max(y_values)))
+    ax1.set_xlim(mini, maxi)
 
     ax2 = fig.add_subplot(312)
     ax2.set_title('predicted')
     ax2.hist(y_values)
-    ax2.set_xlim(min(min(x_values), min(y_values)), max(max(x_values), max(y_values)))
+    ax2.set_xlim(mini, maxi)
 
     ax3 = fig.add_subplot(313)
     ax3.scatter(x_values, y_values)
-    ax3.axis('equal')
+    ax3.set_ylim(mini, maxi)
+    ax3.set_xlim(mini, maxi)
     plt.show()
     if save:
         plt.savefig('data/' + str(label) + '.png')
